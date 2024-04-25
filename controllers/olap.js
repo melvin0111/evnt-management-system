408dbe90e6ad7078a4b0e70a58179c5242a8f8fb
Haris Aqeel <harisaaqeel@gmail.com>
2024-04-25 07:19:28 +0500

    feat: updated olap

--- controllers/olap.js
+++ controllers/olap.js
@@ -50,7 +50,109 @@ const query = async (req, res) => {
         return failedResponse(res, new HTTP_ERRORS.InternalServerError(error));
     }
 }
+
+const highest_attendance = async (req, res) => {
+    try {
+        const result = await knex.raw(`SELECT e.name, COUNT(*) AS attendance
+        FROM events e
+        INNER JOIN orders o ON e.id = o.event_id
+        user_id = ${req.user.id}
+        GROUP BY e.name
+        ORDER BY attendance DESC
+        LIMIT 1;`)
+        return success(result, res, "200", "Success");
+    } catch (error) {
+        console.error("Error creating event:", error);
+        return failedResponse(res, new HTTP_ERRORS.InternalServerError(error));
+    }
+}
+const most_selling_ticket = async (req, res) => {
+    try {
+        const result = await knex.raw(`select event_name , ticket_type from (
+            SELECT e.name as event_name , t.ticket_type as ticket_type,count( o.quantity ) as quantity , RANK() over (partition by e.name ORDER BY count( o.quantity )) as ranking
+            FROM events e
+            INNER JOIN tickets t ON e.id = t.event_id
+            INNER JOIN orders o ON e.id = o.event_id
+            where user_id = ${req.user.id}
+            GROUP BY e.name,t.ticket_type) as temp where temp.ranking =1;`)
+        return success(result, res, "200", "Success");
+    } catch (error) {
+        console.error("Error creating event:", error);
+        return failedResponse(res, new HTTP_ERRORS.InternalServerError(error));
+    }
+}
+const most_revenue_ticket = async (req, res) => {
+    try {
+        const result = await knex.raw(`select event_name , ticket_type from (
+            SELECT e.name as event_name , t.ticket_type as ticket_type,SUM( o.total_amount ) as amount , RANK() over (partition by e.name ORDER BY SUM( o.total_amount ) ) as ranking
+            FROM events e
+            INNER JOIN tickets t ON e.id = t.event_id
+            INNER JOIN orders o ON e.id = o.event_id
+            where user_id = ${req.user.id}
+            GROUP BY e.name,t.ticket_type) as temp where temp.ranking =1;`)
+        return success(result, res, "200", "Success");
+    } catch (error) {
+        console.error("Error creating event:", error);
+        return failedResponse(res, new HTTP_ERRORS.InternalServerError(error));
+    }
+}
+
+const calender_anaylysis = async (req, res) => {
+    try {
+        const result = await knex.raw(`SELECT YEAR(e.start_date) AS year, MONTH(e.start_date) AS month, COUNT(*) AS num_events, SUM(o.total_amount) AS total_revenue
+        FROM events e
+        JOIN orders o ON e.id = o.event_id
+        where user_id = ${req.user.id}
+        GROUP BY YEAR(e.start_date), MONTH(e.start_date)
+        ORDER BY year, month;`)
+        return success(result, res, "200", "Success");
+    } catch (error) {
+        console.error("Error creating event:", error);
+        return failedResponse(res, new HTTP_ERRORS.InternalServerError(error));
+    }
+}
+
+const location_analysis = async (req, res) => {
+    try {
+        const result = await knex.raw(`SELECT location->>"$.city" AS City, 
+        COUNT(distinct e.id ) AS NumberOfEvents,
+        COALESCE(SUM(ex.amount), 0) AS TotalExpenses
+        FROM events e
+        LEFT JOIN expense ex ON e.id = ex.event_id
+        where user_id = ${req.user.id}
+        GROUP BY location->>"$.city" 
+        ORDER BY NumberOfEvents DESC;`)
+        return success(result, res, "200", "Success");
+    } catch (error) {
+        console.error("Error creating event:", error);
+        return failedResponse(res, new HTTP_ERRORS.InternalServerError(error));
+    }
+}
+
+const avg_ticket_price = async (req, res) => {
+    try {
+        const result = await knex.raw(`SELECT e.name AS EventName, 
+        AVG(t.price) AS AverageTicketPrice
+      FROM events e
+      JOIN tickets t ON e.id = t.event_id
+      GROUP BY e.name
+      ORDER BY AverageTicketPrice DESC;`)
+        return success(result, res, "200", "Success");
+    } catch (error) {
+        console.error("Error creating event:", error);
+        return failedResponse(res, new HTTP_ERRORS.InternalServerError(error));
+    }
+}
+
+
+
 module.exports = {
     upcomingEvents,
-    revenue_and_expenses
+    revenue_and_expenses,
+    highest_attendance,
+    most_selling_ticket,
+    most_revenue_ticket,
+    calender_anaylysis,
+    location_analysis,
+    avg_ticket_price
 }
--- routes/olap.js
+++ routes/olap.js
@@ -4,6 +4,12 @@ const Router = require('express').Router();
 
 Router.get('/upcoming', olap.upcomingEvents)
 Router.get('/revenueandexpenses', validateToken, olap.revenue_and_expenses)
+Router.get('/eventwithhighestattendance', validateToken, olap.highest_attendance)
+Router.get('/mostsellingticket', validateToken, olap.most_selling_ticket)
+Router.get('/mostsrevenuegeneratingticket', validateToken, olap.most_revenue_ticket)
+Router.get('/calender_analysis', validateToken, olap.calender_anaylysis)
+Router.get('/location_analysis', validateToken, olap.location_analysis)
+Router.get('/avg_ticket_price', validateToken, olap.avg_ticket_price)
 
 
 module.exports = Router;
