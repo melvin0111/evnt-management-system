450fdf6f8d47bb9d731227b9ee81e345b62bbb97
Haris Aqeel <harisaaqeel@gmail.com>
2024-04-25 08:05:03 +0500

    feat: update

--- controllers/olap.js
+++ controllers/olap.js
@@ -32,24 +32,6 @@ const revenue_and_expenses = async (req, res) => {
         return failedResponse(res, new HTTP_ERRORS.InternalServerError(error));
     }
 }
-const query = async (req, res) => {
-    try {
-        const result = await knex.raw(`SQL
-        SELECT e.name, 
-               SUM(o.total_amount) AS revenue, 
-               SUM(ex.amount) AS expenses
-        FROM events e
-        LEFT JOIN orders o ON e.id = o.event_id
-        LEFT JOIN expense ex ON e.id = ex.event_id
-        where user_id = ${req.user.id}
-        GROUP BY e.name
-        ORDER BY e.name;`)
-        return success(result, res, "200", "Success");
-    } catch (error) {
-        console.error("Error creating event:", error);
-        return failedResponse(res, new HTTP_ERRORS.InternalServerError(error));
-    }
-}
 
 const highest_attendance = async (req, res) => {
     try {
--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,6 @@ const { validateToken } = require('../midlewares');
 const Router = require('express').Router();
 Router.post('/login', auth.login)
 Router.post('/register', auth.register)
-Router.post('/event', validateToken,)
 
 
 
